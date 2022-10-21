e7142d102e5b583c3d849dff35d6a2c7bb262103
Robert Mccreary <fooyinfighter@gmail.com>
2022-10-20 18:19:43 -0700

    integrate new styles into Rooms

--- src/components/Header.js
+++ src/components/Header.js
@@ -24,14 +24,6 @@ function Header() {
           </div>
         </div>
       </header>
-      <div className="masthead p-5 mb-4 bg-light rounded-3">
-        <div className="container-fluid py-5">
-          <h1 className="display-5 fw-bold">Welcome to Your Party</h1>
-          <p className="col-md-8 fs-4">Using a series of utilities, you can create this jumbotron, just like the one in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your liking.</p>
-          <input className="flex-md-nowrap p-3 my-3 form-control form-control-dark w-100" type="text" placeholder="Create a Room" aria-label="Search" />
-          <button className="btn btn-primary btn-lg" type="button">Create</button>
-        </div>
-      </div>
 
     </>
   );
