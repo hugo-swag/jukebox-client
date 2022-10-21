e611835a2cbcaf8db5022f220cb70bbdacc5855f
Robert Mccreary <fooyinfighter@gmail.com>
2022-10-20 17:54:04 -0700

    convert html tags for react jsx

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,25 +12,24 @@ import '../styles/Header.css'
 function Header() {
   return (
     <>
-      <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-        <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="/">Jukebox Hero</a>
-        <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse"
+      <header className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+        <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="/">Jukebox Hero</a>
+        <button className="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse"
           data-bs-target="/sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search" />
-        <div class="navbar-nav">
-          <div class="nav-item text-nowrap">
-            <a class="nav-link px-3" href="/">Sign out</a>
+        <input className="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search" /> <div className="navbar-nav">
+          <div className="nav-item text-nowrap">
+            <a className="nav-link px-3" href="/">Sign out</a>
           </div>
         </div>
       </header>
-      <div class="masthead p-5 mb-4 bg-light rounded-3">
-        <div class="container-fluid py-5">
-          <h1 class="display-5 fw-bold">Welcome to Your Party</h1>
-          <p class="col-md-8 fs-4">Using a series of utilities, you can create this jumbotron, just like the one in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your liking.</p>
-          <input class="flex-md-nowrap p-3 my-3 form-control form-control-dark w-100" type="text" placeholder="Create a Room" aria-label="Search" />
-          <button class="btn btn-primary btn-lg" type="button">Create</button>
+      <div className="masthead p-5 mb-4 bg-light rounded-3">
+        <div className="container-fluid py-5">
+          <h1 className="display-5 fw-bold">Welcome to Your Party</h1>
+          <p className="col-md-8 fs-4">Using a series of utilities, you can create this jumbotron, just like the one in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your liking.</p>
+          <input className="flex-md-nowrap p-3 my-3 form-control form-control-dark w-100" type="text" placeholder="Create a Room" aria-label="Search" />
+          <button className="btn btn-primary btn-lg" type="button">Create</button>
         </div>
       </div>
 
